263fec89144dbe25e7881980996c9aadf34120a0
Dilmer-Rojas <dilmer.rojas@ukuepa.com>
2025-06-28 21:24:29 -0500

    PT_SQL_DA

---
+++ src/02-KPIs-del-Negocio/2.kpis_del_negocio.sql
@@ -0,0 +1,15 @@
+/*
+File: 2.kpis_del_negocio.sql
+Author: Laureano Rojas Castañeda
+Date: 28/06/2025
+Description:
+    Cálculo de KPIs en la base de datos 'shopmax.db'.
+Usage:
+    Ejecutar en base de datos 'shopmax.db'.
+Dependencies:
+    - customers
+	- products
+    - orders
+	- order_items
+*/
+
