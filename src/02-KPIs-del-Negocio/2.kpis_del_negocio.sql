23e87c09b44fff1985ffe36fb6d14152a14e54b3
Dilmer-Rojas <dilmer.rojas@ukuepa.com>
2025-06-29 11:16:42 -0500

    adsadsad

--- src/02-KPIs-del-Negocio/2.kpis_del_negocio.sql
+++ src/02-KPIs-del-Negocio/2.kpis_del_negocio.sql
@@ -13,3 +13,10 @@ Dependencies:
 	- order_items
 */
 
+-- 01. Calcula el ingreso promedio por pedido.
+
+
+-- 02. Encuentra los 5 clientes que más han gastado desde que se unieron.
+
+
+-- 03. Obtén la cantidad de pedidos hechos por cada cliente junto con su nombre.
